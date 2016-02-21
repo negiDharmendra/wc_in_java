cd0b5a5bdf7e2cbd2c0debea8e279df4955a48b7
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-02-21 22:38:18 +0530

    added charCount method in Wc class and wrote three test in WcTest class

--- build/worldCount.sh
+++ build/worldCount.sh
@@ -1,4 +1,4 @@
-alias rt="java -cp ./test:./out:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore WorldCountTest"
-alias ct="javac -cp .:lib/junit-4.12.jar -d out test/WorldCountTest.java"
+alias rt="java -cp ./test:./out:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore WcTest"
+alias ct="javac -cp .:lib/junit-4.12.jar -d out test/WcTest.java"
 alias cm="javac matrix/WorldCount.java"
 source ~/.bash_profile
