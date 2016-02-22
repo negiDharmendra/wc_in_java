52c0bb519fade30ac58e01e261272ffd2809d5a0
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-02-22 19:07:11 +0530

    [Dharam] implemented few method (getMethodName,getOption, 	convertOptionIntoMethodName) in 	CommandLineArgsReader class to get methodNames 	according to provided options.

--- source/CommandLineArgsReader.java
+++ source/CommandLineArgsReader.java
@@ -1,12 +1,43 @@
 package source;
+import java.util.Arrays;
 
 public class CommandLineArgsReader {
 	private String [] args;
     public CommandLineArgsReader(String [] args){
     	this.args = args;
     }
-    public String [] getMethodName(){
-    	String [] result = {"lineCount"};
+    private String [] getOption (){
+    	int index = 0,newIndex = 0;
+    	String [] options = new String [args.length];
+    	while(args[index].charAt(0)=='-'&&args[index].length()>1){
+    		options[index]=args[index];
+    		index++;
+    	}
+    	String [] result = new String [index];
+    	for (String option : options)
+    		if(option!=null){
+    			result[newIndex] = option;
+    			newIndex++;
+    		}
     	return result;
+    }
+    private String [] convertOptionIntoMethodName(String [] options){
+    	String methodNames = "";
+    	for (String option : options){
+    		if(option=="-l")
+    			methodNames+="lineCount\n";
+    		if(option=="-w")
+    			methodNames+="wordCount\n";
+    		if(option=="-m")
+    			methodNames+="charCount\n";
+    		if(option=="-c")
+    			methodNames+="byteCount\n";
+    	}
+    	return methodNames.split("\\n");
+    }
+    public String [] getMethodName(){
+    	String [] options = getOption();
+    	String [] methodNames = convertOptionIntoMethodName(options);
+    	return methodNames;
     }
 }
