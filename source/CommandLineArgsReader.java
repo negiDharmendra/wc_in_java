49bcd85e3436a6aa756fcd1b4557fa6873b4f5cb
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-02-23 08:30:11 +0530

    wrote test for invalid options and a method filterInvalidOptions to handle those invalid options

--- source/CommandLineArgsReader.java
+++ source/CommandLineArgsReader.java
@@ -1,5 +1,7 @@
 package source;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CommandLineArgsReader {
 	private String [] args;
@@ -23,20 +25,30 @@ public class CommandLineArgsReader {
     }
     private String [] convertOptionIntoMethodName(String [] options){
     	String methodNames = "";
-    	for (String option : options){
-    		if(option=="-l")
-    			methodNames+="lineCount\n";
-    		if(option=="-w")
-    			methodNames+="wordCount\n";
-    		if(option=="-m")
-    			methodNames+="charCount\n";
-    		if(option=="-c")
-    			methodNames+="byteCount\n";
-    	}
+        String allOptions = Arrays.toString(options);
+		if(allOptions.indexOf("-l")>=0)
+			methodNames+="lineCount\n";
+		if(allOptions.indexOf("-w")>=0)
+			methodNames+="wordCount\n";
+		if(allOptions.lastIndexOf("-c")>allOptions.lastIndexOf("-m"))
+			methodNames+="byteCount\n";
+		if(allOptions.lastIndexOf("-c")<allOptions.lastIndexOf("-m"))
+			methodNames+="charCount\n";
     	return methodNames.split("\\n");
+    }
+    private void filterInvalidOptions(String [] options){
+        String pattern = "[^lwcm]";
+        String optionWithoutHyphen = "";
+        for (int i=0; i<options.length; i++)
+            optionWithoutHyphen += options[i].substring(1);
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(optionWithoutHyphen);
+        if(matcher.find())
+            throw new IllegalArgumentException("\n\twc: illegal option -- "+matcher.group(0)+"\n\tusage: wc [-clmw] [file ...]");
     }
     public String [] getMethodName(){
     	String [] options = getOption();
+        filterInvalidOptions(options);
     	String [] methodNames = convertOptionIntoMethodName(options);
     	return methodNames;
     }
--- test/CommandLineArgsReaderTest.java
+++ test/CommandLineArgsReaderTest.java
@@ -9,10 +9,47 @@ import source.CommandLineArgsReader;
 
 public class CommandLineArgsReaderTest {
 	@Test
-    public void getMethodNameShouldGiveAllTheMethodNameAccordingToOptionProvidedToWc(){
+    public void getMethodNameShouldGiveTheMethodNameAccordingToProvidedOption(){
     	String [] args = {"-l","sample.txt"};
     	CommandLineArgsReader commandLineArgs = new CommandLineArgsReader(args);
     	String [] methods = {"lineCount"};
     	assertArrayEquals(methods,commandLineArgs.getMethodName());
+    }
+    @Test
+    public void getMethodNameShouldGiveAllTheMethodNamesAccordingToProvidedOptions(){
+    	String [] args = {"-l","-w","-m","sample.txt"};
+    	CommandLineArgsReader commandLineArgs = new CommandLineArgsReader(args);
+    	String [] methods = {"lineCount","wordCount","charCount"};
+    	assertArrayEquals(methods,commandLineArgs.getMethodName());
+    }
+    @Test
+    public void getMethodNameShouldNotRepeatTheMethodNames(){
+        String [] args = {"-l","-w","-m","-l","-w","-m","-l","-w","-m","-l","-w","-m","-l","-w","-m","sample.txt"};
+        CommandLineArgsReader commandLineArgs = new CommandLineArgsReader(args);
+        String [] methods = {"lineCount","wordCount","charCount"};
+        assertArrayEquals(methods,commandLineArgs.getMethodName());
+    }
+    @Test
+    public void getMethodNameShouldSelectTheLastMethodAmongTheByteCountOrCharCount(){
+        String [] args = {"-l","-w","-m","-c","sample.txt"};
+        CommandLineArgsReader commandLineArgs = new CommandLineArgsReader(args);
+        String [] methods = {"lineCount","wordCount","byteCount"};
+        assertArrayEquals(methods,commandLineArgs.getMethodName());
+
+        String [] args1 = {"-l","-w","-c","-m","sample.txt"};
+        commandLineArgs = new CommandLineArgsReader(args1);
+        String [] methods1 = {"lineCount","wordCount","charCount"};
+        assertArrayEquals(methods1,commandLineArgs.getMethodName());
+    }
+    @Test
+    public void getMethodNameShouldThrowAnErrorForInvalidOptions(){
+        String [] args = {"-l","-q","-m","sample.txt"};
+        CommandLineArgsReader commandLineArgs = new CommandLineArgsReader(args);
+        String errorMessage = "\n\twc: illegal option -- q\n\tusage: wc [-clmw] [file ...]";
+        try {
+            commandLineArgs.getMethodName();
+        } catch (IllegalArgumentException err) {
+            assertEquals(err.getMessage(), errorMessage);
+        }
     }
 }
